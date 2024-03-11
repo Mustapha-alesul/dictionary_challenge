e7edb949ebc833ea401a7b657ebaeeb5cbbed5a5
Mustapha-alesul <mustapha.bdsh@gmail.com>
2024-03-11 19:04:34 +0200

    emailsss

--- emails.py
+++ emails.py
@@ -1,8 +1,9 @@
 "A python challenge to sort out emails sent messages from  A given list"
 
 filee = open("mbox.txt","r")
-line = set()
+emails = {}
 for file in filee:
     if file.startswith("From"):
         line = file.split()[1]
-print(line)
+        emails.get(line,0) + 1
+print(emails)
