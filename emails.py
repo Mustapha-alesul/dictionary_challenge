ffec630c1fed1f4f12f2ee712452470ab464836e
Mustapha-alesul <mustapha.bdsh@gmail.com>
2024-03-11 22:04:37 +0200

    trying days

--- emails.py
+++ emails.py
@@ -2,12 +2,16 @@
 import re
 filee = open("mbox.txt","r")
 emails = {}
+days = {}
 for file in filee:
     if file.startswith("From"):
         line = file.split()[1]
+        day = file.split()[2]
         emails[line] = emails.get(line,0) + 1
+        days[day] = days.get(day, 0) + 1
         maxx = max(emails)
         domainn = maxx[maxx.index('@')+1 : maxx.index(".")]
 print(emails)
 print("maximum:", maxx)
 print('domain:', domainn)
+print(days)
