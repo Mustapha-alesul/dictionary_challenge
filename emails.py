b742013fad42c87e47e3b43f438cd86ceae06b58
Mustapha-alesul <mustapha.bdsh@gmail.com>
2024-03-12 10:43:53 +0200

    recover

--- emails.py
+++ emails.py
@@ -1,1 +1,18 @@
-"a challenge"
+"A python challenge to sort out emails sent messages from  A given list"
+
+filee = open("mbox.txt","r")
+emails = {}
+days = {}
+for file in filee:
+    if file.startswith("From"):
+        line = file.split()[1]
+        day = file.split()[2]
+        emails[line] = emails.get(line,0) + 1
+        days[day] = days.get(day, 0) + 1
+        maxx = max(emails)
+        domain_name = maxx[maxx.index('@')+1 : maxx.index(".")]
+print(emails)
+print("maximum:", maxx)
+print('domain:', domainn)
+print(day)
+filee.close()
