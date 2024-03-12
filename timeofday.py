3d5df213cbbf8831b1c02fe5b6de86c290856b63
Mustapha-alesul <mustapha.bdsh@gmail.com>
2024-03-12 20:37:45 +0200

    line is cought well

--- timeofday.py
+++ timeofday.py
@@ -3,4 +3,8 @@ with open('mbox.txt','r') as filee:
     for file in filee:
         if file.startswith("From"):
             try:
-                line = file.split()[]
+                line = file.split()[5]
+
+            except IndexError:
+                continue
+            print(line)
